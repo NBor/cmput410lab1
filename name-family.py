610a51e4978f12a9ae8581b95b41fee0fef5f52f
Neil Borle <nborle@ualberta.ca>
2015-01-14 18:00:28 -0700

    updated student class

--- name-family.py
+++ name-family.py
@@ -1,20 +1,50 @@
 class Student(object):
 
+    """ Class to model a student.
+
+    Stores the student's first and last name as attributes. Also stores a
+    dictionary of all the students courses and marks.
+
+    """
+
     def __init__(self, name, family):
+        """Function to calc average gpa.
+
+        >>> student = Student('John', 'Smith')
+        >>> student.name
+        'John'
+        >>> student.family
+        'Smith'
+
+        """
         self.course_marks = {}
         self.name = name
         self.family = family
 
     def add_course_mark(self, course, mark):
+        """Function to calc average gpa.
+
+        >>> student = Student('John', 'Smith')
+        >>> student.add_course_mark('french', 2.0)
+        >>> student.course_marks
+        {'french': 2.0}
+
+        """
         self.course_marks[course] = mark
 
     def average(self):
+        """Function to calc average gpa.
+
+        >>> student = Student('John', 'Smith')
+        >>> student.add_course_mark('french', 2.0)
+        >>> student.add_course_mark('german', 3.0)
+        >>> student.add_course_mark('english', 4.0)
+        >>> student.average()
+        3.0
+        """
         total = sum(self.course_marks.values())
         return total/float(len(self.course_marks))
 
 if __name__ == '__main__':
-    student = Student('John', 'Smith')
-    student.add_course_mark('french', 2.0)
-    student.add_course_mark('german', 3.0)
-    student.add_course_mark('english', 4.0)
-    print student.average()
+    import doctest
+    doctest.testmod()
